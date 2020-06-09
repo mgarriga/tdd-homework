d8c042fc3dc5ca4d42e4d674c405cbbaf726d1a9
Martin Garriga <mgarriga87@gmail.com>
2020-06-08 22:25:59 -0300

    gutter game test GREEN
    Gutter game (all rolls zero) passing test. Refactor still needed.

--- test-bowling.py
+++ test-bowling.py
@@ -4,5 +4,11 @@ import pytest
 def game():
     return Game()
 
+def play_bowling(current_score, rolls, pins):
+    return 0
+
 def test_gutter_game(game):
-    assert game.get_score() == 0
+    result = play_bowling(current_score=0, rolls=20, pins=0)
+    assert result == 0
+
+def test_all_ones(game)
